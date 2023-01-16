4f0af401d0f6fd71930410346d8c3899452b446a
Demaria Woods <dwoods447@gmail.com>
2023-01-15 21:05:41 -0600

    added origin to cors

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,10 @@
+import cors from 'cors'
 import { createServer } from './utils/server.js'
 import defaultConfig from './config/default'
-import cors from 'cors'
+
 const app = createServer()
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-app.use(cors())
+app.use(cors({origin: 'https://63c4bc8b3a64455afde13b40--spiffy-chimera-cf83fd.netlify.app/'}))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/test', (req, res) => res.status(200).json({message: 'This is a message from the server'}))
 
